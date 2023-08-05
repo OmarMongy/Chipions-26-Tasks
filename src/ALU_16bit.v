e3f318e977f32b625733e6a42220482f9474f3d2
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-08-05 23:08:27 +0300

    Create ALU_16bit.v

---
+++ src/ALU_16bit.v
@@ -0,0 +1,20 @@
+module ALU(
+  input [7:0] x, y,
+  input [2:0] sel,
+  output reg [15:0] z
+);
+wire overflow;
+always @(*)
+begin
+    case (sel)
+      3'b000 : z = x + y;
+      3'b001 : z = x - y;
+      3'b010 : z = x * y;
+      3'b011 : z = { x % y, x / y };
+      3'b100 : z = x | y;
+      3'b101 : z = x & y;
+      3'b110 : z = ~x;
+      3'b111 : z = ~y;
+    endcase
+end
+endmodule 
